bca529bbfc61d5dbab94d73caaa221f0c7c66a95
wenjie.zhang <wenjie.zhang@raiing.com>
2017-05-12 16:23:54 +0800

    首次提交

---
+++ js/wenjie.js
@@ -0,0 +1,53 @@
+var keys = document.querySelectorAll('#calculator span');
+var operators = ['+', '-', 'x', '÷'];
+var decimalAdded = false;
+
+for(var i = 0; i < keys.length; i++) {
+	keys[i].onclick = function(e) {
+		var input = document.querySelector('.screen');
+		var inputVal = input.innerHTML;
+		var btnVal = this.innerHTML;
+		if(btnVal == 'C') {
+			input.innerHTML = 0;
+			decimalAdded = false;
+		}
+		else if(btnVal == '=') {
+			var equation = inputVal;
+			var lastChar = equation[equation.length - 1];
+			equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
+			if(operators.indexOf(lastChar) > -1 || lastChar == '.')
+				equation = equation.replace(/.$/, '');
+			if(equation)
+				input.innerHTML = eval(equation);
+			decimalAdded = false;
+		}
+		else if(operators.indexOf(btnVal) > -1) {
+			var lastChar = inputVal[inputVal.length - 1];
+			if(inputVal != '' && operators.indexOf(lastChar) == -1) 
+				input.innerHTML += btnVal;
+			else if(inputVal == '' && btnVal == '-') 
+				input.innerHTML += btnVal;
+			if(operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
+				input.innerHTML = inputVal.replace(/.$/, btnVal);
+			}
+			decimalAdded =false;
+		}
+		else if(btnVal == '.') {
+			if(!decimalAdded) {
+				input.innerHTML += btnVal;
+				decimalAdded = true;
+			}
+		}
+		else if(btnVal == 'ext') {
+			return false;
+		}
+		else {
+			if(input.innerHTML == 0){
+				input.innerHTML = btnVal;
+			}else{
+				input.innerHTML += btnVal;
+			}
+		}
+		e.preventDefault();
+	}
+}
